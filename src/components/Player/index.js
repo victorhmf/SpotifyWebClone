b1d57b4d6ed43e31b2ee8daed9ea70f43542c2ef
Victor Fernandes <victor.hmfd@gmail.com>
2019-05-23 09:17:38 -0300

    play and pause
    Signed-off-by: Victor Fernandes <victor.hmfd@gmail.com>

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -4,6 +4,8 @@ import Sound from 'react-sound';
 import PropTypes from 'prop-types';
 
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Creators as PlayerActions } from '../../store/ducks/player';
 
 import {
   Container, Current, Volume, Progress, Controls, Time, ProgressSlider,
@@ -16,16 +18,13 @@ import PauseIcon from '../../assets/images/pause.svg';
 import ForwardIcon from '../../assets/images/forward.svg';
 import RepeatIcon from '../../assets/images/repeat.svg';
 
-const Player = ({ player }) => (
+const Player = ({ player, play, pause }) => (
   <Container>
     {!!player.currentSong && <Sound url={player.currentSong.file} playStatus={player.status} />}
     <Current>
       {!!player.currentSong && (
         <Fragment>
-          <img
-            src={player.currentSong.thumbnail}
-            alt={player.currentSong.title}
-          />
+          <img src={player.currentSong.thumbnail} alt={player.currentSong.title} />
           <div>
             <span>{player.currentSong.title}</span>
             <small>{player.currentSong.author}</small>
@@ -41,9 +40,16 @@ const Player = ({ player }) => (
         <button type="button">
           <img src={BackwardIcon} alt="Backward" />
         </button>
-        <button type="button">
-          <img src={PlayIcon} alt="Play" />
-        </button>
+        {!!player.currentSong && player.status === Sound.status.PLAYING ? (
+          <button type="button" onClick={pause}>
+            <img src={PauseIcon} alt="Pause" />
+          </button>
+        ) : (
+          <button type="button" onClick={play}>
+            <img src={PlayIcon} alt="Play" />
+          </button>
+        )}
+
         <button type="button">
           <img src={ForwardIcon} alt="Forward" />
         </button>
@@ -86,10 +92,17 @@ Player.propTypes = {
     }),
     status: PropTypes.string,
   }).isRequired,
+  play: PropTypes.func.isRequired,
+  pause: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
   player: state.player,
 });
 
-export default connect(mapStateToProps)(Player);
+const mapDispatchToProps = dispatch => bindActionCreators(PlayerActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Player);
--- src/store/ducks/player.js
+++ src/store/ducks/player.js
@@ -2,6 +2,8 @@ import Sound from 'react-sound';
 
 export const Types = {
   LOAD: 'player/LOAD',
+  PLAY: 'player/PLAY',
+  PAUSE: 'player/PAUSE',
 };
 
 const INITIAL_STATE = {
@@ -14,6 +16,12 @@ export default function player(state = INITIAL_STATE, action) {
     case Types.LOAD:
       return { ...state, currentSong: action.payload.song, status: Sound.status.PLAYING };
 
+    case Types.PLAY:
+      return { ...state, status: Sound.status.PLAYING };
+
+    case Types.PAUSE:
+      return { ...state, status: Sound.status.PAUSED };
+
     default:
       return state;
   }
@@ -23,5 +31,13 @@ export const Creators = {
   loadSong: song => ({
     type: Types.LOAD,
     payload: { song },
+  }),
+
+  play: () => ({
+    type: Types.PLAY,
+  }),
+
+  pause: () => ({
+    type: Types.PAUSE,
   }),
 };
