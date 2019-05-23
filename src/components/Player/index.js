f60b0c0dc4d4df78328b2caf5d2dcfa2163f4071
Victor Fernandes <victor.hmfd@gmail.com>
2019-05-23 09:34:00 -0300

    next and prev
    Signed-off-by: Victor Fernandes <victor.hmfd@gmail.com>

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -18,9 +18,9 @@ import PauseIcon from '../../assets/images/pause.svg';
 import ForwardIcon from '../../assets/images/forward.svg';
 import RepeatIcon from '../../assets/images/repeat.svg';
 
-const Player = ({ player, play, pause }) => (
+const Player = ({ player, play, pause, next, prev }) => (
   <Container>
-    {!!player.currentSong && <Sound url={player.currentSong.file} playStatus={player.status} />}
+    {!!player.currentSong && <Sound url={player.currentSong.file} playStatus={player.status} onFinishedPlaying={next} />}
     <Current>
       {!!player.currentSong && (
         <Fragment>
@@ -37,7 +37,7 @@ const Player = ({ player, play, pause }) => (
         <button type="button">
           <img src={ShuffleIcon} alt="Shuffle" />
         </button>
-        <button type="button">
+        <button type="button" onClick={prev}>
           <img src={BackwardIcon} alt="Backward" />
         </button>
         {!!player.currentSong && player.status === Sound.status.PLAYING ? (
@@ -50,7 +50,7 @@ const Player = ({ player, play, pause }) => (
           </button>
         )}
 
-        <button type="button">
+        <button type="button" onClick={next}>
           <img src={ForwardIcon} alt="Forward" />
         </button>
         <button type="button">
@@ -94,6 +94,8 @@ Player.propTypes = {
   }).isRequired,
   play: PropTypes.func.isRequired,
   pause: PropTypes.func.isRequired,
+  next: PropTypes.func.isRequired,
+  prev: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
--- src/pages/Playlist/index.js
+++ src/pages/Playlist/index.js
@@ -104,7 +104,7 @@ class Playlist extends Component {
                 <SongItem
                   key={song.id}
                   onClick={() => this.setState({ selectedSong: song.id })}
-                  onDoubleClick={() => loadSong(song)}
+                  onDoubleClick={() => loadSong(song, playlistDetails.data.songs)}
                   selected={selectedSong === song.id}
                   playing={currentSong && currentSong.id === song.id}
                 >
